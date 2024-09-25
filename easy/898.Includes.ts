e9998c8ef2d8e3ac0f2cb8a4c6cabb4241357f6d
m_sakaguchi <m_sakaguchi@guthrie.jp>
2024-09-25 13:43:04 +0900

    回答

---
+++ easy/898.Includes.ts
@@ -0,0 +1,8 @@
+type Includes<T extends readonly any[], U> = T extends [
+    infer First,
+    ...infer Rest
+]
+    ? Equal<First, U> extends true
+        ? true
+        : Includes<Rest, U>
+    : false;
